9314d55280a95195b3e1e6b75124843ac0831198
Raygun <raygun200@gmail.com>
2025-01-18 12:39:09 +0530

    one more origin added for cors

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],  # Replace "*" with specific origins for production
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:55959"],  # Replace "*" with specific origins for production
     allow_credentials=True,
     allow_methods=["GET", "POST", "OPTIONS"],  # Allow all HTTP methods
     allow_headers=["Content-Type", "Authorization"],  # Allow all headers
