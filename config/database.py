d9d7f8ef43bdfa3503937de3a84faba495cca0fa
Raygun <raygun200@gmail.com>
2025-01-20 20:32:25 +0530

    Connected to Global Database Uri

--- config/database.py
+++ config/database.py
@@ -2,8 +2,8 @@ from pymongo import MongoClient
 
 # MongoDB connection setup
 def get_db():
-    uri = "mongodb://localhost:27017/"
-    # uri = "mongodb+srv://nilaymathur:U3BQfgNWufbmr7Cd@cluster0.aoghk.mongodb.net/myDatabase?retryWrites=true&w=majority"
+    # uri = "mongodb://localhost:27017/"
+    uri = "mongodb+srv://nilaymathur:U3BQfgNWufbmr7Cd@cluster0.aoghk.mongodb.net/myDatabase?retryWrites=true&w=majority"
     client = MongoClient(uri)
     db = client["Properties"]
     return db
