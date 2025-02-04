c5b262190e237a9168d7443164583d69354e0350
Nilay Mathur <nilaymathur@gmail.com>
2025-02-04 11:04:09 +0530

    Updated database.py

--- config/database.py
+++ config/database.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
-import certifi
 
 # MongoDB connection setup
 def get_db():
     # uri = "mongodb://localhost:27017/"
     uri = "mongodb+srv://nilaymathur:U3BQfgNWufbmr7Cd@cluster0.aoghk.mongodb.net/myDatabase?retryWrites=true&w=majority"
-    client = MongoClient(uri, tls=True, tlsCAFile=certifi.where())
+    client = MongoClient(uri)
     db = client["Properties"]
+    print(uri);
     return db
+
